9339414e8f50e8d3770f106d72e6517bd5749831
SeijiOmi <50559028+SeijiOmi@users.noreply.github.com>
2021-05-22 12:13:12 +0900

    Userモデルを作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,28 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { UsersModule } from './user/users.module';
+import { CustomNamingStrategy } from './customNamingStrategy';
+import { User } from './user/user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: process.env.DB_HOST,
+      port: parseInt(process.env.DB_PORT),
+      database: process.env.DB_NAME,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      synchronize: false,
+      entities: [User],
+      namingStrategy: new CustomNamingStrategy(),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
